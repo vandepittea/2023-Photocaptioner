d2ed32ce7361d8e0b40a1156ac32c598ea057030
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2023-03-31 00:26:25 +0200

    Add font styling

--- app/src/main/java/com/example/photocaptioner/ui/theme/Type.kt
+++ app/src/main/java/com/example/photocaptioner/ui/theme/Type.kt
@@ -2,27 +2,54 @@ package com.example.photocaptioner.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import com.example.photocaptioner.R
+
+// Initialize downloaded fonts
+val NunitoSans = FontFamily(
+    Font(R.font.nunito_sans_regular),
+    Font(R.font.nunito_sans_bold, FontWeight.Bold)
+)
+
+val AbrilFatface = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = AbrilFatface,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontSize = 12.sp,
+        letterSpacing = 1.15.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = NunitoSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.15.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = NunitoSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        letterSpacing = 0.5.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NunitoSans,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 10.sp,
+        letterSpacing = 0.4.sp
+    ),
+    button = TextStyle(
+        fontFamily = NunitoSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        letterSpacing = 1.sp,
+        textAlign = TextAlign.Center
     )
-    */
 )
