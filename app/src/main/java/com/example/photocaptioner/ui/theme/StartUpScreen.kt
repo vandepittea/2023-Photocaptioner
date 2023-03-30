79b1fef797af014bb0aa1f325c1c5cb6e013d66a
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2023-03-31 00:45:55 +0200

    Add the startup screen

---
+++ app/src/main/java/com/example/photocaptioner/ui/theme/StartUpScreen.kt
@@ -0,0 +1,74 @@
+package com.example.photocaptioner.ui.theme
+
+import androidx.annotation.StringRes
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.example.photocaptioner.R
+
+@Composable
+fun StartUpScreen(@StringRes title: Int, image: Int) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+        )
+
+        Text(
+            text = stringResource(title),
+            style = MaterialTheme.typography.h1,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp)
+        )
+
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.onPrimary
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp)
+                .height(60.dp)
+        ) {
+            Text(
+                text = "Get Started",
+                style = MaterialTheme.typography.button
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun StartUpScreenPreview(){
+    StartUpScreen(
+        R.string.app_name,
+        R.drawable.camera
+    )
+}
