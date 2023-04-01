30ae88a406b9042d415ae0a8883683b22a1d4a24
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2023-04-01 12:32:38 +0200

    Update photo class

--- app/src/main/java/com/example/photocaptioner/model/Photo.kt
+++ app/src/main/java/com/example/photocaptioner/model/Photo.kt
@@ -2,10 +2,11 @@ package com.example.photocaptioner.model
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import java.time.LocalDate
 import java.util.*
 
 data class Photo(
     @DrawableRes val image: Int,
     @StringRes val description: Int,
-    val createdAt: Date
+    val createdAt: LocalDate
 )
