7456aca32e7af149b681287baad48466d9d0d317
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2023-04-04 00:38:17 +0200

    Optimize code

--- app/src/main/java/com/example/photocaptioner/ui/AlbumDetailScreen.kt
+++ app/src/main/java/com/example/photocaptioner/ui/AlbumDetailScreen.kt
@@ -21,139 +21,198 @@ import com.example.photocaptioner.model.Album
 import com.example.photocaptioner.R
 
 @Composable
-fun AlbumDetailScreen(album: Album) {
-    val colors = MaterialTheme.colors
-
+fun AlbumDetailScreen(
+    album: Album,
+    onDownloadClick: () -> Unit,
+    onEditClick: () -> Unit,
+    onAddClick: () -> Unit,
+    onShareClick: () -> Unit,
+) {
     Box(
         Modifier
             .fillMaxSize()
-            .background(colors.background)
+            .background(MaterialTheme.colors.background)
     ) {
         Column(
-            Modifier.fillMaxSize()
+            Modifier
+                .fillMaxSize()
                 .padding(horizontal = 16.dp, vertical = 8.dp)
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = stringResource(id = album.name),
-                    style = MaterialTheme.typography.subtitle1,
-                    color = colors.onBackground,
-                    modifier = Modifier
-                        .padding(bottom = 4.dp)
-                        .weight(6f)
-                )
-
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                ) {
-                    IconButton(
-                        onClick = {}
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.baseline_download_24),
-                            contentDescription = stringResource(R.string.edit_icon),
-                            tint = colors.onBackground,
-                            modifier = Modifier.
-                            size(45.dp)
-                        )
-                    }
-
-                    IconButton(
-                        onClick = {}
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Edit,
-                            contentDescription = stringResource(R.string.edit_icon),
-                            tint = colors.onBackground,
-                            modifier = Modifier.
-                            size(45.dp)
-                        )
-                    }
-                }
-            }
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(bottom = 16.dp)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_description_24),
-                    contentDescription = stringResource(id = R.string.description_icon),
-                    tint = colors.onBackground,
-                    modifier = Modifier.size(40.dp)
-                )
-
-                Spacer(modifier = Modifier.width(8.dp))
-
-                Text(
-                    text = stringResource(id = album.description),
-                    style = MaterialTheme.typography.body1,
-                    color = colors.onBackground
-                )
-            }
-
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(bottom = 16.dp)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_access_time_filled_24),
-                    contentDescription = stringResource(id = R.string.time_icon),
-                    tint = colors.onBackground,
-                    modifier = Modifier.size(32.dp)
-                )
-
-                Spacer(modifier = Modifier.width(8.dp))
-
-                Text(
-                    text = "${album.lastChanged}",
-                    style = MaterialTheme.typography.body2,
-                    color = colors.onBackground,
-                )
-            }
-
-            AlternatingColumn(items = album.photos)
+            AlbumDetails(
+                album = album,
+                onDownloadClick = onDownloadClick,
+                onEditClick = onEditClick,
+            )
         }
 
         Column(
-            Modifier.align(Alignment.BottomCenter)
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
                 .fillMaxWidth()
-                .background(colors.background)
+                .background(MaterialTheme.colors.background)
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 10.dp, bottom = 10.dp),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                IconButton(
-                    onClick = {}
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_add_24),
-                        contentDescription = stringResource(R.string.add_icon),
-                        tint = colors.onBackground,
-                        modifier = Modifier.size(45.dp)
-                    )
-                }
+            AlbumFooter(
+                onAddClick = onAddClick,
+                onShareClick = onShareClick,
+            )
+        }
+    }
+}
 
-                IconButton(
-                    onClick = {}
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.baseline_share_24),
-                        contentDescription = stringResource(R.string.share_icon),
-                        tint = colors.onBackground,
-                        modifier = Modifier.size(45.dp)
-                    )
-                }
+@Composable
+fun AlbumDetails(
+    album: Album,
+    onDownloadClick: () -> Unit,
+    onEditClick: () -> Unit,
+) {
+    AlbumHeader(
+        name = stringResource(id = album.name),
+        onDownloadClick = onDownloadClick,
+        onEditClick = onEditClick,
+    )
+
+    DescriptionRow(
+        description = stringResource(id = album.description),
+    )
+
+    TimeRow(
+        lastChanged = "${album.lastChanged}",
+    )
+
+    AlternatingColumn(
+        items = album.photos
+    )
+}
+
+@Composable
+fun AlbumHeader(
+    name: String,
+    onDownloadClick: () -> Unit,
+    onEditClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = name,
+            style = MaterialTheme.typography.subtitle1,
+            color = MaterialTheme.colors.onBackground,
+            modifier = Modifier
+                .padding(bottom = 4.dp)
+                .weight(6f)
+        )
+
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            IconButton(
+                onClick = onDownloadClick,
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_download_24),
+                    contentDescription = stringResource(R.string.download_icon),
+                    tint = MaterialTheme.colors.onBackground,
+                    modifier = Modifier.size(45.dp)
+                )
             }
+
+            IconButton(
+                onClick = onEditClick,
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.edit_icon),
+                    tint = MaterialTheme.colors.onBackground,
+                    modifier = Modifier.size(45.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DescriptionRow(
+    description: String,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(bottom = 16.dp)
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_description_24),
+            contentDescription = stringResource(id = R.string.description_icon),
+            tint = MaterialTheme.colors.onBackground,
+            modifier = Modifier.size(40.dp)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = description,
+            style = MaterialTheme.typography.body1,
+            color = MaterialTheme.colors.onBackground
+        )
+    }
+}
+
+@Composable
+fun TimeRow(
+    lastChanged: String
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_access_time_filled_24),
+            contentDescription = stringResource(id = R.string.time_icon),
+            tint = MaterialTheme.colors.onBackground,
+            modifier = Modifier.size(32.dp)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = lastChanged,
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.onBackground,
+        )
+    }
+}
+
+@Composable
+fun AlbumFooter(
+    onAddClick: () -> Unit,
+    onShareClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 10.dp, bottom = 10.dp),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(
+            onClick = onAddClick
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_add_24),
+                contentDescription = stringResource(R.string.add_icon),
+                tint = MaterialTheme.colors.onBackground,
+                modifier = Modifier.size(45.dp)
+            )
+        }
+
+        IconButton(
+            onClick = onShareClick
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_share_24),
+                contentDescription = stringResource(R.string.share_icon),
+                tint = MaterialTheme.colors.onBackground,
+                modifier = Modifier.size(45.dp)
+            )
         }
     }
 }
@@ -162,6 +221,6 @@ fun AlbumDetailScreen(album: Album) {
 @Composable
 fun AlbumDetailScreenPreview(){
     PhotoCaptionerTheme {
-        AlbumDetailScreen(Datasource.albumList[0])
+        AlbumDetailScreen(Datasource.albumList[0], {}, {}, {}, {})
     }
 }
