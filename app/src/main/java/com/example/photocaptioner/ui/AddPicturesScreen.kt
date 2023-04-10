c0743c0b239e3daa60f57846d80c9c7d7d592870
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2023-04-11 01:48:00 +0200

    Optimize code

--- app/src/main/java/com/example/photocaptioner/ui/AddPicturesScreen.kt
+++ app/src/main/java/com/example/photocaptioner/ui/AddPicturesScreen.kt
@@ -12,15 +12,10 @@ import com.example.photocaptioner.model.MapsPhoto
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.example.photocaptioner.R
 import com.example.photocaptioner.ui.theme.PhotoCaptionerTheme
 
