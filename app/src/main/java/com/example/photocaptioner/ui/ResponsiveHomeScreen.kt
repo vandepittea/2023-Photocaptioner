ceb25b4ae61e5603896ecb32d4c5eb9b05e4cdf1
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2023-04-17 11:53:44 +0200

    Optimize code

--- app/src/main/java/com/example/photocaptioner/ui/ResponsiveHomeScreen.kt
+++ app/src/main/java/com/example/photocaptioner/ui/ResponsiveHomeScreen.kt
@@ -97,7 +97,47 @@ fun ResponsiveHomeScreen(
                     }
                 },
                 modifier = Modifier.testTag(navigationDrawerContentDescription)
-            ){}
+            ){
+                InAppNavigation(
+                    navController,
+                    onStartUpClick,
+                    onTakePictureClick,
+                    onGoToAlbumsClick,
+                    recentlyEdited,
+                    albumList,
+                    onAddAlbumClick,
+                    onAlbumClick,
+                    detailedAlbum,
+                    onDownloadClick,
+                    onEditClick,
+                    onAddPictureClick,
+                    onShareClick,
+                    onPhotoClick,
+                    onChooseCamera,
+                    onChooseGallery,
+                    onChooseMaps,
+                    searchValue,
+                    searchedPhotos,
+                    onSearchChanged,
+                    onImageSelected,
+                    onPictureUploadButtonClick,
+                    newPhotos,
+                    newTitle,
+                    newDescription,
+                    onAlbumTitleAdd,
+                    onAlbumDescriptionAdd,
+                    onAddNewAlbum,
+                    albumToEdit,
+                    onAlbumTitleChange,
+                    onAlbumDescriptionChange,
+                    onAlbumSave,
+                    photoToEdit,
+                    photoDescriptionToEdit,
+                    onPhotoDescriptionChange,
+                    onPhotoSave,
+                    modifier
+                )
+            }
         }
 
         Column(
@@ -105,94 +145,45 @@ fun ResponsiveHomeScreen(
                 .fillMaxSize()
                 .background(MaterialTheme.colors.background)
         ) {
-            NavHost(
-                navController = navController,
-                startDestination = PhotoCaptionerScreen.Start.name,
-                modifier = modifier
-            ) {
-                composable(PhotoCaptionerScreen.Start.name) {
-                    StartUpScreen(
-                        onButtonClick = onStartUpClick
-                    )
-                }
-
-                composable(PhotoCaptionerScreen.Home.name) {
-                    HomeScreen(
-                        onTakePictureClick = onTakePictureClick,
-                        onAlbumsClick = onGoToAlbumsClick,
-                        recentlyEdited = recentlyEdited
-                    )
-                }
-
-                composable(PhotoCaptionerScreen.Albums.name) {
-                    AlbumsScreen(
-                        albumList = albumList,
-                        onAddClick = onAddAlbumClick,
-                        onAlbumClick = onAlbumClick
-                    )
-                }
-
-                composable(PhotoCaptionerScreen.AlbumDetail.name) {
-                    AlbumDetailScreen(
-                        album = detailedAlbum,
-                        onDownloadClick = onDownloadClick,
-                        onEditClick = onEditClick,
-                        onAddClick = onAddPictureClick,
-                        onShareClick = onShareClick,
-                        onPhotoClick = onPhotoClick
-                    )
-                }
-
-                composable(PhotoCaptionerScreen.ChoosePicturesSource.name) {
-                    ChoosePicturesSourceScreen(
-                        onChooseCamera = onChooseCamera,
-                        onChooseGallery = onChooseGallery,
-                        onChooseMaps = onChooseMaps
-                    )
-                }
-
-                composable(PhotoCaptionerScreen.AddPictures.name) {
-                    AddPicturesScreen(
-                        searchValue = searchValue,
-                        searchedPhotos = searchedPhotos,
-                        onSearchChanged = onSearchChanged,
-                        onImageSelected = onImageSelected,
-                        onUploadButtonClick = onPictureUploadButtonClick
-                    )
-                }
-
-                composable(PhotoCaptionerScreen.AddAlbum.name) {
-                    AddAlbumsScreen(
-                        newPhotos = newPhotos,
-                        newTitle = newTitle,
-                        newDescription = newDescription,
-                        onAlbumTitleChange = onAlbumTitleAdd,
-                        onAlbumDescriptionChange = onAlbumDescriptionAdd,
-                        onChooseCamera = onChooseCamera,
-                        onChooseGallery = onChooseGallery,
-                        onChooseMaps = onChooseMaps,
-                        onAddNewAlbum = onAddNewAlbum
-                    )
-                }
-
-                composable(PhotoCaptionerScreen.EditAlbum.name) {
-                    EditAlbumScreen(
-                        albumToEdit = albumToEdit,
-                        onAlbumTitleChange = onAlbumTitleChange,
-                        onAlbumDescriptionChange = onAlbumDescriptionChange,
-                        onSave = onAlbumSave
-                    )
-                }
-
-                composable(PhotoCaptionerScreen.EditPhoto.name) {
-                    EditPhotoScreen(
-                        photoToEdit = photoToEdit,
-                        description = photoDescriptionToEdit,
-                        onPhotoDescriptionChange = onPhotoDescriptionChange,
-                        onSave = onPhotoSave
-                    )
-                }
-            }
+            InAppNavigation(
+                navController,
+                onStartUpClick,
+                onTakePictureClick,
+                onGoToAlbumsClick,
+                recentlyEdited,
+                albumList,
+                onAddAlbumClick,
+                onAlbumClick,
+                detailedAlbum,
+                onDownloadClick,
+                onEditClick,
+                onAddPictureClick,
+                onShareClick,
+                onPhotoClick,
+                onChooseCamera,
+                onChooseGallery,
+                onChooseMaps,
+                searchValue,
+                searchedPhotos,
+                onSearchChanged,
+                onImageSelected,
+                onPictureUploadButtonClick,
+                newPhotos,
+                newTitle,
+                newDescription,
+                onAlbumTitleAdd,
+                onAlbumDescriptionAdd,
+                onAddNewAlbum,
+                albumToEdit,
+                onAlbumTitleChange,
+                onAlbumDescriptionChange,
+                onAlbumSave,
+                photoToEdit,
+                photoDescriptionToEdit,
+                onPhotoDescriptionChange,
+                onPhotoSave,
+                modifier
+            )
 
             AnimatedVisibility(visible = navigationType == PhotoCaptionerNavigationType.BOTTOM_NAVIGATION) {
                 val bottomNavigationContentDescription = stringResource(R.string.navigation_bottom)
@@ -203,6 +194,136 @@ fun ResponsiveHomeScreen(
                     modifier = Modifier.testTag(bottomNavigationContentDescription)
                 )
             }
+        }
+    }
+}
+
+@Composable
+private fun InAppNavigation(
+    navController: NavHostController,
+    onStartUpClick: () -> Unit,
+    onTakePictureClick: () -> Unit,
+    onGoToAlbumsClick: () -> Unit,
+    recentlyEdited: Photo,
+    albumList: List<Album> = emptyList(),
+    onAddAlbumClick: () -> Unit,
+    onAlbumClick: (Album) -> Unit,
+    detailedAlbum: Album,
+    onDownloadClick: () -> Unit,
+    onEditClick: () -> Unit,
+    onAddPictureClick: () -> Unit,
+    onShareClick: () -> Unit,
+    onPhotoClick: (Photo) -> Unit,
+    onChooseCamera: () -> Unit,
+    onChooseGallery: () -> Unit,
+    onChooseMaps: () -> Unit,
+    searchValue: String,
+    searchedPhotos: List<Pair<Boolean, MapsPhoto>>,
+    onSearchChanged: (String) -> Unit,
+    onImageSelected: (Int) -> Unit,
+    onPictureUploadButtonClick: () -> Unit,
+    newPhotos: List<Photo> = emptyList(),
+    newTitle: String,
+    newDescription: String,
+    onAlbumTitleAdd: (String) -> Unit,
+    onAlbumDescriptionAdd: (String) -> Unit,
+    onAddNewAlbum: () -> Unit,
+    albumToEdit: Album,
+    onAlbumTitleChange: (String) -> Unit,
+    onAlbumDescriptionChange: (String) -> Unit,
+    onAlbumSave: () -> Unit,
+    photoToEdit: Photo,
+    photoDescriptionToEdit: String,
+    onPhotoDescriptionChange: (String) -> Unit,
+    onPhotoSave: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = PhotoCaptionerScreen.Start.name,
+        modifier = modifier
+    ) {
+        composable(PhotoCaptionerScreen.Start.name) {
+            StartUpScreen(
+                onButtonClick = onStartUpClick
+            )
+        }
+
+        composable(PhotoCaptionerScreen.Home.name) {
+            HomeScreen(
+                onTakePictureClick = onTakePictureClick,
+                onAlbumsClick = onGoToAlbumsClick,
+                recentlyEdited = recentlyEdited
+            )
+        }
+
+        composable(PhotoCaptionerScreen.Albums.name) {
+            AlbumsScreen(
+                albumList = albumList,
+                onAddClick = onAddAlbumClick,
+                onAlbumClick = onAlbumClick
+            )
+        }
+
+        composable(PhotoCaptionerScreen.AlbumDetail.name) {
+            AlbumDetailScreen(
+                album = detailedAlbum,
+                onDownloadClick = onDownloadClick,
+                onEditClick = onEditClick,
+                onAddClick = onAddPictureClick,
+                onShareClick = onShareClick,
+                onPhotoClick = onPhotoClick
+            )
+        }
+
+        composable(PhotoCaptionerScreen.ChoosePicturesSource.name) {
+            ChoosePicturesSourceScreen(
+                onChooseCamera = onChooseCamera,
+                onChooseGallery = onChooseGallery,
+                onChooseMaps = onChooseMaps
+            )
+        }
+
+        composable(PhotoCaptionerScreen.AddPictures.name) {
+            AddPicturesScreen(
+                searchValue = searchValue,
+                searchedPhotos = searchedPhotos,
+                onSearchChanged = onSearchChanged,
+                onImageSelected = onImageSelected,
+                onUploadButtonClick = onPictureUploadButtonClick
+            )
+        }
+
+        composable(PhotoCaptionerScreen.AddAlbum.name) {
+            AddAlbumsScreen(
+                newPhotos = newPhotos,
+                newTitle = newTitle,
+                newDescription = newDescription,
+                onAlbumTitleChange = onAlbumTitleAdd,
+                onAlbumDescriptionChange = onAlbumDescriptionAdd,
+                onChooseCamera = onChooseCamera,
+                onChooseGallery = onChooseGallery,
+                onChooseMaps = onChooseMaps,
+                onAddNewAlbum = onAddNewAlbum
+            )
+        }
+
+        composable(PhotoCaptionerScreen.EditAlbum.name) {
+            EditAlbumScreen(
+                albumToEdit = albumToEdit,
+                onAlbumTitleChange = onAlbumTitleChange,
+                onAlbumDescriptionChange = onAlbumDescriptionChange,
+                onSave = onAlbumSave
+            )
+        }
+
+        composable(PhotoCaptionerScreen.EditPhoto.name) {
+            EditPhotoScreen(
+                photoToEdit = photoToEdit,
+                description = photoDescriptionToEdit,
+                onPhotoDescriptionChange = onPhotoDescriptionChange,
+                onSave = onPhotoSave
+            )
         }
     }
 }
