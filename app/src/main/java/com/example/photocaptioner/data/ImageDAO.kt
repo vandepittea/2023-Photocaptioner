767f2079e34167fbe9e7203ace5a8eaf3209edeb
Timixo <51677515+Timixo@users.noreply.github.com>
2023-05-31 12:01:49 +0200

    Added Images DAO

---
+++ app/src/main/java/com/example/photocaptioner/data/ImageDAO.kt
@@ -0,0 +1,16 @@
+package com.example.photocaptioner.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ImageDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(image: Image)
+
+    @Query("SELECT * FROM images WHERE albumId = :albumId")
+    fun getImages(albumId: Long): Flow<List<Image>>
+}
