4236aa9b0bb4f119c504ff26cdf93ad56ec2dc81
Timixo <51677515+Timixo@users.noreply.github.com>
2023-05-31 12:39:35 +0200

    Added update function

--- app/src/main/java/com/example/photocaptioner/data/ImageDAO.kt
+++ app/src/main/java/com/example/photocaptioner/data/ImageDAO.kt
@@ -4,12 +4,16 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ImageDAO {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(image: Image)
+
+    @Update
+    suspend fun update(image: Image)
 
     @Query("SELECT * FROM images WHERE albumId = :albumId")
     fun getImages(albumId: Long): Flow<List<Image>>
