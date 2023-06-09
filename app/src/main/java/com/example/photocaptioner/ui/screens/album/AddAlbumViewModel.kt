a7db95b040eefd46a83551a891914d5187894517
Tim De Jonghe <tim.de.jonghe@student.howest.be>
2023-06-09 12:36:51 +0200

    Small refactor

--- app/src/main/java/com/example/photocaptioner/ui/screens/album/AddAlbumViewModel.kt
+++ app/src/main/java/com/example/photocaptioner/ui/screens/album/AddAlbumViewModel.kt
@@ -55,7 +55,7 @@ class AddAlbumViewModel(
     }
 
     suspend fun deletePhotosWithoutAlbum() {
-        run {
+        withContext(Dispatchers.IO) {
             albumsRepository.deletePhotosWithoutAlbum()
         }
     }
