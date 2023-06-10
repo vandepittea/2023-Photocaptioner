5f78cd7d9f9afa06092c64c43e078807585b3d3d
Tim De Jonghe <tim.de.jonghe@student.howest.be>
2023-06-10 23:18:46 +0200

    Added tests because of extra functionality

--- app/src/test/java/com/example/photocaptioner/ui/screens/PhotoCaptionersViewModelTest.kt
+++ app/src/test/java/com/example/photocaptioner/ui/screens/PhotoCaptionersViewModelTest.kt
@@ -8,15 +8,7 @@ class PhotoCaptionersViewModelTest {
     private val viewModel = PhotoCaptionersViewModel()
 
     @Test
-    fun gameViewModel_Initialization() {
-        val gameUiState = viewModel.uiState.value
-
-        Assert.assertTrue(gameUiState.currentMenuItem == MenuItemType.Home)
-        Assert.assertFalse(gameUiState.canNavigateBack)
-    }
-
-    @Test
-    fun gameViewModel_UpdateCurrentMenuItem() {
+    fun photoCaptionersViewModel_UpdateCurrentMenuItem() {
         viewModel.updateCurrentMenuItem(MenuItemType.Albums)
         val gameUiState = viewModel.uiState.value
 
@@ -25,11 +17,31 @@ class PhotoCaptionersViewModelTest {
     }
 
     @Test
-    fun gameViewModel_CanNavigateBack() {
+    fun photoCaptionersViewModel_CanNavigateBack() {
         viewModel.canNavigateBack(true)
         val gameUiState = viewModel.uiState.value
 
         Assert.assertTrue(gameUiState.currentMenuItem == MenuItemType.Home)
         Assert.assertTrue(gameUiState.canNavigateBack)
+    }
+
+    @Test
+    fun photoCaptionersViewModel_UpdateTopBarTitle() {
+        viewModel.updateTopBarTitle("Test Title")
+        val gameUiState = viewModel.uiState.value
+
+        Assert.assertTrue(gameUiState.currentMenuItem == MenuItemType.Home)
+        Assert.assertFalse(gameUiState.canNavigateBack)
+        Assert.assertTrue(gameUiState.topBarTitle == "Test Title")
+    }
+
+    @Test
+    fun photoCaptionersViewModel_UpdateBottomBarVisibility() {
+        viewModel.updateBottomBarVisibility(true)
+        val gameUiState = viewModel.uiState.value
+
+        Assert.assertTrue(gameUiState.currentMenuItem == MenuItemType.Home)
+        Assert.assertFalse(gameUiState.canNavigateBack)
+        Assert.assertTrue(gameUiState.bottomBarVisible)
     }
 }
